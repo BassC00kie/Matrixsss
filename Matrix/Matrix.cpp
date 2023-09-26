582e9068afb13084bb38b32f975ed3815071986f
Школа программирования 1 11 <schpr1-11@int.adtspb.ru>
2023-09-26 19:32:43 +0300

    -

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,36 +1,58 @@
 ﻿#include <iostream>
 using namespace std;
+int Size_N1;
+int Size_M1;
+
+int plus(int** mat1, int** mat2) {
+    int** matrixS = new int* [Size_N1];
+    for (int i = 0; i < Size_N1; i++)
+    {
+        matrixS[i] = new int[Size_M1];
+    }
+    mat1 = new int* [Size_N1];
+    for (int i = 0; i < Size_N1; i++) {
+        mat1[i] = new int[Size_M1];
+        for (int j = 0; j < Size_M1; j++) {
+            matrixS[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+    for (int i = 0; i < Size_N1; i++)
+    {
+        for (int j = 0; j < Size_M1; j++)
+        {
+            cout << matrixS[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+int mult(int** mat1, int** mat2) {
+
+}
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    int Size_N1;
-    int Size_M1;
-    int Size_N2;
-    int Size_M2;
-    cout << "Введите кол-во столбцов у первой матрицы" << "\n";
+    
+    cout << "Введите кол-во столбцов у матрицы" << "\n";
     cin >> Size_N1;
-    cout << "Введите кол-во строк у первой матрицы" << "\n";
+    cout << "Введите кол-во строк у матрицы" << "\n";
     cin >> Size_M1;
     int** matrix1 = new int* [Size_N1];
     for (int i = 0; i < Size_N1; i++)
     {
         matrix1[i] = new int[Size_M1];
     }
-    cout << "Введите кол-во столбцов у второй матрицы" << "\n";
-    cin >> Size_N2;
-    cout << "Введите кол-во строк у второй матрицы" << "\n";
-    cin >> Size_M2;
-    int** matrix2 = new int* [Size_N2];
-    for (int i = 0; i < Size_N2; i++)
+    int** matrix2 = new int* [Size_N1];
+    for (int i = 0; i < Size_N1; i++)
     {
-        matrix2[i] = new int[Size_M2];
+        matrix2[i] = new int[Size_M1];
     }
     for (int i = 0; i < Size_N1; i++) {
         for (int j = 0; j < Size_M1; j++)
             matrix1[i][j] = i;
     }
-    for (int i = 0; i < Size_N2; i++) {
-        for (int j = 0; j < Size_M2; j++)
+    for (int i = 0; i < Size_N1; i++) {
+        for (int j = 0; j < Size_M1; j++)
             matrix2[i][j] = i;
     }
     for (int i = 0; i < Size_N1; i++)
@@ -42,12 +64,14 @@ int main()
         cout << '\n';
     }
     cout << '\n';
-    for (int i = 0; i < Size_N2; i++)
+    for (int i = 0; i < Size_N1; i++)
     {
-        for (int j = 0; j < Size_M2; j++)
+        for (int j = 0; j < Size_M1; j++)
         {
             cout << matrix2[i][j] << ' ';
         }
         cout << '\n';
     }
+    plus();
+    return 0;
 }
