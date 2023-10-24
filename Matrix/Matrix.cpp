4bb420b081c98ec488d5e30fe6b090ea8a6b69e9
Школа программирования 1 11 <schpr1-11@int.adtspb.ru>
2023-10-24 18:46:41 +0300

    Plusing finaly

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -4,9 +4,20 @@
 #include <fstream>
 using namespace std;
 int Size_N1;
-int Size_M1 = Size_N1;
+int Size_M1;
 int N;
-int plusing(int** mat1, int** mat2) {
+int** show(int** mat1) {
+    for (int i = 0; i < Size_N1; i++)
+    {
+        for (int j = 0; j < Size_M1; j++)
+        {
+            cout << mat1[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return mat1;
+}
+int** plusing1(int** mat1, int** mat2) {
     int** matrixS = new int* [Size_N1];
     for (int i = 0; i < Size_N1; i++)
     {
@@ -17,25 +28,22 @@ int plusing(int** mat1, int** mat2) {
             matrixS[i][j] = mat1[i][j] + mat2[i][j];
         }
     }
-    for (int i = 0; i < Size_N1; i++)
-    {
-        for (int j = 0; j < Size_M1; j++)
-        {
-            cout << matrixS[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    return 0;
+    return matrixS;
 }
-/*int mult(int** mat1, int** mat2) {
-
-}*/
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    ifstream outfile("U:\'Кириллл В'\Matrixsss\\MatrixN1");
+    ifstream outfile("MatrixN1.txt");
+    if (outfile.is_open())
+        cout << "file has been opened\n\n";
+    else
+    {
+        cout << "file hasn't been open\n\n";
+        return -1;
+    }
     outfile >> Size_N1;
+    Size_M1 = Size_N1;
     int** matrix1 = new int* [Size_N1];
     for (int i = 0; i < Size_N1; i++)
     {
@@ -47,8 +55,9 @@ int main()
         matrix2[i] = new int[Size_M1];
     }
     for (int i = 0; i < Size_N1; i++) {
-        for (int j = 0; j < Size_M1; j++)
+        for (int j = 0; j < Size_M1; j++) {
             outfile >> matrix1[i][j];
+        }
     }
     for (int i = 0; i < Size_N1; i++) {
         for (int j = 0; j < Size_M1; j++)
@@ -76,5 +85,7 @@ int main()
         }
         cout << '\n';
     }
-    plusing(matrix1, matrix2);
+    cout << '\n';
+    int** matrixAns =  plusing1(matrix1, matrix2);
+    show(matrixAns);
 }
